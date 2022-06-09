d2ab3b93f72141ecd79547c828c8a2673cb9dc93
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-09 09:17:25 +0300

    commit tree f9bb441a78c64b07770bba6aae41c05084d9f52f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+//1. findMatching
+//=====================
+
+// function findMatching(array, string) {
+//   const newList = array.filter(
+//     (driver) => driver.toLowerCase() === string.toLowerCase()
+//   );
+//   return newList;
+// }
+
+const findMatching = (array, string) => {
+  const newList = array.filter(
+    (element) => element.toLowerCase() === string.toLowerCase()
+  );
+  return newList;
+};
+
+//2. fuzzyMatch
+//==================
+function fuzzyMatch(array, string) {
+  const fuzzyList = array.filter(
+    (item) => item.toLowerCase().slice(0, 2) === string.toLowerCase()
+  );
+  //   const fuzzyList = array.filter(
+  //     (item) => item.toLowerCase().indexOf(string.toLowerCase(), 0) === 0
+  //   );
+  return fuzzyList;
+}
+
+//3. matchName
+//==============
+function matchName(array, string) {
+  const result = array.filter((element) => element.name === string);
+  return result;
+}
